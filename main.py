1dcd3b0095e2b2d4ec227e44d5e362e8ea9409be
Ajith Kanumuri <saiajith25@gmail.com>
2023-06-20 21:03:20 +0530

    [BugFix] Convert port value type to int

--- main.py
+++ main.py
@@ -173,7 +173,7 @@ async def favicon7():
 
 if __name__ == "__main__":
     uvicorn.run(
-        "main:app",
+        app,
         host="0.0.0.0",
-        port=os.environ.get("PORT", 8000)
+        port=int(os.environ.get("PORT", 8000))
     )
